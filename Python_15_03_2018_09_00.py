7d46f927893ebf35f63bad618b14ee1eacc52d4c
IgorMalutki <ireneuszzadlo@gmail.com>
2018-03-15 13:35:37 +0100

    Python_15_03_2018_13_30

---
+++ Python_15_03_2018_09_00.py
@@ -0,0 +1,165 @@
+
+from random import sample
+
+'''
+def L500 (dochody,l_dzieci,l_głów):
+
+    decision = {True:'Otrzymałeś świadczenie', False:'Niestety nie otrzymałeś świadczenia'}
+    świadczenie = 0
+    dochód_na_głowe = dochody / l_głów
+
+    if (l_dzieci >= 2 and dochód_na_głowe > 800):
+        #decision = True
+        świadczenie = (l_dzieci-1)*500
+        return (decision[True],świadczenie)
+    elif(l_dzieci>=2 and dochód_na_głowe<=800):
+        #decision = True
+        świadczenie = l_dzieci * 500
+        return (decision[True], świadczenie)
+    elif(l_dzieci < 2 and dochód_na_głowe > 800):
+        #decision = False
+        świadczenie = 0
+        return (decision[False], świadczenie)
+    else:
+        #decision = True
+        świadczenie = 500
+        return (decision[True], świadczenie)
+
+
+
+
+def walidacjaWe():
+    print('Witaj w programie 500+')
+    while(True):
+        try:
+            dochody = float(input('Podaj dochód rodziny: '))
+            l_dzieci = int(input('Podaj liczbę dzieci :'))
+            l_głów = int(input('Podaj wielkość rodziny'))
+        except:
+            print('Podałeś złe dane :')
+            continue
+        if(dochody >=0 and l_dzieci >= 0 and l_głów > l_dzieci):
+            print(L500(dochody, l_dzieci, l_głów))
+            break
+        else:
+            print('ERROR')
+
+walidacjaWe()
+
+#P72
+
+def odl(p1,p2):
+
+    wynik = (((p2[0]-p1[0])**2)+((p2[1]-p1[1])**2))**(1/2)
+    return wynik
+
+p1=(int(input('Podaj x1')),int(input('Podaj y1')))
+p2=(int(input('Podaj x2')),int(input('Podaj y2')))
+#p1 = (1,1)
+#p2 = (1,1)
+print(odl(p1,p2))
+
+#P74
+def avg(*arg):
+    if(len(arg) == 0):
+        return 0
+    wynik = 0
+    for elem in arg:
+        wynik += elem
+    return wynik/len(arg)
+
+print(avg(0))
+
+
+
+
+#p76
+
+def html(color_name = 'red',font_size = 15,content = 'Lorem Ipsum'):
+    #return('<span style="color:'+ color_name +';font-size:'+font_size+';“>'+content+'</span>')
+    return'<span style="color: %s;font-size:%s;“> %s </span>'% (color_name,font_size,content)
+
+
+
+color = input('color')
+size = input('size')
+text = input('text')
+if  (color=='' and size=='' and text==''):
+    print(html())
+else:
+    print(html(color,size,text))
+
+
+
+#P77 zmienna globalna
+
+def blackORwhite ():
+    global color
+    if (color == 'white'):
+        color = 'black'
+        return color
+    else:
+        color = 'white'
+        return color
+
+color = 'white'
+
+
+print(blackORwhite())
+print(blackORwhite())
+print(blackORwhite())
+print(blackORwhite())
+print(blackORwhite())
+print(blackORwhite())
+print(blackORwhite())
+print(blackORwhite())
+
+
+#p79
+
+wynik = 0
+
+def potega(x,y):
+    global wynik
+    wynik = (x**y)
+
+potega(3,3)
+print(wynik)
+
+napis = 'Lorem Ipsum momento mori'
+def my_reverse(a):
+    global napis
+    napis = ''
+    for i,v in enumerate(a):
+        napis+= a[(len(a)-1)-i]
+
+my_reverse(napis)
+print(napis)
+
+def wordReverse(a):
+    global napis
+    napis = ' '
+    a=a.split(' ')
+    for i,v in enumerate(a):
+        napis+= a[(len(a)-1)-i]+' '
+
+
+napis = 'Lorem Ipsum momento mori'
+wordReverse(napis)
+print(napis)
+'''
+
+
+#P80
+
+def sum_spec(min_wsad,licze):
+    zbior = sample(range(0,1000,1),licze)
+    lista = []
+    suma = 0
+    for elem in zbior:
+        if elem>=min_wsad:
+            lista.append(elem)
+            suma +=elem
+    return lista,suma
+
+print(sum_spec(1,10))
